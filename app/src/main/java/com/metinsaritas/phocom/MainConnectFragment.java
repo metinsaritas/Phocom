c6118f6d57e096a81d7de3c585c03a87f1868425
Metin SARITAŞ <metinsaritas2@gmail.com>
2019-03-26 18:33:27 -0700

    QRCode Reader setup

--- app/src/main/java/com/metinsaritas/phocom/MainConnectFragment.java
+++ app/src/main/java/com/metinsaritas/phocom/MainConnectFragment.java
@@ -1,20 +1,77 @@
 package com.metinsaritas.phocom;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ActivityCompat;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.metinsaritas.phocom.Types.FragmentWithTitle;
 
-public class MainConnectFragment extends FragmentWithTitle {
+import java.io.IOException;
+
+public class MainConnectFragment extends FragmentWithTitle implements SurfaceHolder.Callback {
+
+    final int RequestCameraPermissionID = 1001;
+    private CameraSource cameraSource;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_connect, container, false);
+        View v = inflater.inflate(R.layout.fragment_main_connect, container, false);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        SurfaceView fmcSvQrCamera = v.findViewById(R.id.fmcSvQrCamera);
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(getContext())
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        cameraSource = new CameraSource.Builder(getContext(), barcodeDetector)
+                .setRequestedPreviewSize(width, height)
+                .build();
+
+        fmcSvQrCamera.getHolder().addCallback(this);
+        return v;
     }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    Manifest.permission.CAMERA
+            }, RequestCameraPermissionID);
+            return;
+        }
+
+        try {
+            cameraSource.start(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (cameraSource != null)
+            cameraSource.stop();
+    }
 }
